aebacb71ebd86d4ccd73eb9a41ce091aeb4e3971
pradnyakolap <pkolap06@gmail.com>
2025-02-14 08:26:12 +0000

    commit

---
+++ basic/04-comparision.js
@@ -0,0 +1,39 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+//************************** output**************************
+//true
+//true
+//false
+//false
+//true
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+//javascript convert string to number then compare  
+//*********************Output***************
+//true
+//true
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null== 0);//we compare 0 with null or undefined we get unexpected result 
+console.log(null < 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+//***************************Output************************** */
+// false
+// true
+// false
+// false
+// false
+// false
+// false
+//false
+console.log("2"=== 2);//false
