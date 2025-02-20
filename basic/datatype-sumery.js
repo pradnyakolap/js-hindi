4cca760c39317b4f48265ed78dd0ba13fa714afd
pradnyakolap <pkolap06@gmail.com>
2025-02-20 09:05:18 +0000

    commit

---
+++ basic/datatype-sumery.js
@@ -0,0 +1,35 @@
+
+/*primitive
+// 7 type of datatype
+1 string= "" quote
+2 number=
+3 boolean =
+4 null=
+5 undefined =
+6 symbol=
+7 bigInt 
+
+*/
+const  score = 100
+const scoreValue =100.3
+const IsloggedIn =false
+const OutsideTemt = null
+let userEmail;
+const id =Symbol("123")
+const anatherId =Symbol("123")
+console.log(id===anatherId);
+//const bigNumer= 1234556788979847678973838256856787878n
+
+//refrence(Non primitive)
+//aaray,object,Function
+const heroes=("shaktiman","doga","nagaarjun");
+let myObj={
+    name:"pradnya",
+    age: 26,
+}
+const myfunction = function(){
+    console.log("hello shailesh");
+
+}
+console.log (typeof bigNumber);
+//https://262.ecma-international.org/5.1/#sec-11.4.3
