c54c3dc933352c1597aa2f6e4af83714822520e3
pradnyakolap <pkolap06@gmail.com>
2025-02-21 09:01:17 +0000

    commit

---
+++ basic/string.js
@@ -0,0 +1,22 @@
+const name ="pradnya"
+const sname ="kamble"
+//console.log(name+sname);
+console.log("hellow my name is ${pradnya}sname is${kamble}");
+const GameName = new String("candycrash")
+// console.log(GameName[0]);
+// console.log(GameName.__proto__);
+// console.log(GameName.length);
+// console.log(GameName.toUpperCase);
+console.log(GameName.charAt(3));
+console.log(GameName.indexOf('d'));
+const  newString=GameName.substring(0,4)
+console.log(newString);
+const anotherstring=GameName.slice(-8,4)
+console.log(anotherstring);
+const  newStringone="  pradnya  "
+console.log(newString);
+console.log(newString.trim());
+const url="http//pradnya.com/pradnya%20kamble"
+console.log(url.replace("%20","-"));
+console.log(url.includes("shailesh"));
+console.log(url.split('-'));
